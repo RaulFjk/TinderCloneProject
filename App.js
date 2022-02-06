d60fd15fd13f1de6fc77db6efc1507c957d7e802
raulfjk <dumaraul13@gmail.com>
2022-02-06 20:49:38 +0100

    Refactor AnimatedStack

--- App.js
+++ App.js
@@ -1,145 +1,28 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import {
-  Text,
-  Image,
-  ImageBackground,
-  View,
-  StyleSheet,
-  Pressable,
-  useWindowDimensions,
-} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Card from './src/components/TinderCard';
 import users from './assets/data/users';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withSpring,
-  useAnimatedGestureHandler,
-  useDerivedValue,
-  interpolate,
-  runOnJS,
-} from 'react-native-reanimated';
-import {PanGestureHandler} from 'react-native-gesture-handler';
-import Like from './assets/images/LIKE.png';
-import Nope from './assets/images/nope.png';
-
-const ROTATION = 60;
-const SWIPE_VELOCITY = 800;
+import AnimatedStack from './src/components/AnimatedStack';
 
 const App = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
 
-  const currentProfile = users[currentIndex];
-  const nextProfile = users[nextIndex];
+  const onSwipeLeft = (user) => {
+    console.warn("swipe left: ", user.name)
+  };
 
-  const {width: screenWidth} = useWindowDimensions();
-
-  const hiddenTranslateX = 2 * screenWidth;
-
-  //translateX is actually the position of the card
-  const translateX = useSharedValue(0); //can go from -width to 0 to +width
-  const rotate = useDerivedValue(
-    () =>
-      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
-      'deg',
-  );
-
-  const cardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: translateX.value,
-      },
-      {
-        rotate: rotate.value,
-      },
-    ],
-  }));
-
-  const nextCardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        scale: interpolate(
-          translateX.value,
-          [-hiddenTranslateX, 0, hiddenTranslateX],
-          [1, 0.8, 1],
-        ),
-      },
-    ],
-    opacity: interpolate(
-      translateX.value,
-      [-hiddenTranslateX, 0, hiddenTranslateX],
-      [1, 0.5, 1],
-    ),
-  }));
-
-  const likeStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
-  }));
-
-  const nopeStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
-  }));
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_, context) => {
-      context.startX = translateX.value;
-    },
-    onActive: (event, context) => {
-      translateX.value = context.startX + event.translationX;
-      // console.log('Touch x: ', event.translationX);
-    },
-    onEnd: event => {
-      //Check how fast the user swipes
-      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
-        //then bring card back in the middle / withSpring will make sure that the transition will not jump but will go smooth
-        translateX.value = withSpring(0);
-
-        return;
-      }
-
-      translateX.value = withSpring(
-        //we multiply the hiddentranslateX with the sign of the velocity ("-" if is user swipes left and "+" if user swipes right)
-        hiddenTranslateX * Math.sign(event.velocityX),
-        {},
-        //when the animation finishes (goes out of the screen) then be we use the callback function to update the index
-        () => runOnJS(setCurrentIndex)(currentIndex + 1),
-      );
-    },
-  });
-
-  useEffect(() => {
-    translateX.value = 0;
-    setNextIndex(currentIndex + 1);
-  }, [currentIndex, translateX]);
+  const onSwipeRight = (user) => {
+    console.warn("swipe right: ", user.name)
+  }
 
   return (
     <View style={styles.pageContainer}>
-      {nextProfile && (
-        <View style={styles.nextCardContainer}>
-          <Animated.View style={[styles.animatedCard, nextCardStyle]}>
-            <Card user={nextProfile} />
-          </Animated.View>
-        </View>
-      )}
-      {currentProfile && (
-        <PanGestureHandler onGestureEvent={gestureHandler}>
-          <Animated.View style={[styles.animatedCard, cardStyle]}>
-            <Animated.Image
-              source={Like}
-              style={[styles.like, {left: 10}, likeStyle]}
-              resizeMode="contain"
-            />
-            <Animated.Image
-              source={Nope}
-              style={[styles.like, {right: 10}, nopeStyle]}
-              resizeMode="contain"
-            />
-            <Card user={currentProfile} />
-          </Animated.View>
-        </PanGestureHandler>
-      )}
+      <AnimatedStack
+        data={users}
+        renderItem={({item}) => <Card user={item} />}
+        onSwipeLeft={onSwipeLeft}
+        onSwipeRight={onSwipeRight}
+      />
     </View>
   );
 };
@@ -149,27 +32,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-  },
-  animatedCard: {
-    width: '90%',
-    height: '70%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  nextCardContainer: {
-    ...StyleSheet.absoluteFillObject,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  like: {
-    width: 150,
-    height: 150,
-    position: 'absolute',
-    top: 10,
-    zIndex: 1,
-    elevation: 20,
   },
 });
 
